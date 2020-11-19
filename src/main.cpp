2883de2aaee843a6fef91f68349b99269448c620
Rachit Mittal <rachit18302@iiitd.ac.in>
2020-11-20 03:54:08 +0530

    triangulation almost donegit add .

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "poly2tri.h"
 #include <set>
 #include <iostream>
+#include <math.h>
 // GLobal variables
 std::vector<float> controlPoints;
 int width = 640, height = 640; 
@@ -15,13 +16,17 @@ p2t::Point* p;
 
 void pushPoint(std::vector<p2t::Point*> & points,p2t::Point* p){
     p2t::Point* prevPoint;
+    double minDist = 0.001;
     if (points.empty()){
         points.push_back(p);
     }
     else{
         prevPoint = points.back();
-        if (!(prevPoint->x==p->x && prevPoint->y==p->y)){
+        double distance = sqrt(pow(p->x-prevPoint->x,2) + pow(prevPoint->y-p->y,2));
+        
+        if (!((prevPoint->x==p->x && prevPoint->y==p->y) || (distance<=minDist))){
             points.push_back(p);
+            // std::cout<<distance<<std::endl;
         }
     }
 }
@@ -44,10 +49,10 @@ int main(int, char* argv[])
 	glUseProgram(shaderProgram);
 
     // Create VBOs, VAOs
-    // unsigned int VBO_controlPoints;
-    // unsigned int VAO_controlPoints;
-    // glGenBuffers(1, &VBO_controlPoints);
-    // glGenVertexArrays(1, &VAO_controlPoints);
+    unsigned int VBO_controlPoints;
+    unsigned int VAO_controlPoints;
+    glGenBuffers(1, &VBO_controlPoints);
+    glGenVertexArrays(1, &VAO_controlPoints);
 
     unsigned int VBO_triangles;
     unsigned int VAO_triangles;
@@ -111,63 +116,70 @@ int main(int, char* argv[])
             rescaled_y = -1.0 + ((1.0*(height - y) - 0) / (height - 0)) * (1.0 - (-1.0));
             p = new p2t::Point(rescaled_x,rescaled_y); 
             pushPoint(points,p);
-            // std::cout<<p->x<<p->y<<std::endl;
-            // points[0]->set_zero();
             controlPointsUpdated = true;
             mouseUppedOnce = false;
-            // removeSimilarPoints(points);
 
             cdt = new p2t::CDT(points);
             cdt->Triangulate();
     		triangles = cdt->GetTriangles();
             for (auto triangle: triangles){
                 for (int i =0;i<3;i++){
-                    // rescaled_x = -1.0 + ((1.0*triangle->GetPoint(i)->x - 0) / (width - 0)) * (1.0 - (-1.0));
-                    // rescaled_y = -1.0 + ((1.0*(height - triangle->GetPoint(i)->y) - 0) / (height - 0)) * (1.0 - (-1.0));
                     triangleFlattenedArray.push_back(triangle->GetPoint(i)->x);
                     triangleFlattenedArray.push_back(triangle->GetPoint(i)->y);
                     triangleFlattenedArray.push_back(0);
                 }
-                // std::cout<<triangle->GetPoint(0)->x<<" "<<triangle->GetPoint(0)->y<<std::endl;
-                // std::cout<<triangle->a<<" "<<triangle->b<<" "<< triangle->c<<std::endl;
+                triangleFlattenedArray.push_back(triangle->GetPoint(0)->x);
+                triangleFlattenedArray.push_back(triangle->GetPoint(0)->y);
+                triangleFlattenedArray.push_back(0);
+                triangleFlattenedArray.push_back(triangle->GetPoint(2)->x);
+                triangleFlattenedArray.push_back(triangle->GetPoint(2)->y);
+                triangleFlattenedArray.push_back(0);
+                triangleFlattenedArray.push_back(triangle->GetPoint(1)->x);
+                triangleFlattenedArray.push_back(triangle->GetPoint(1)->y);
+                triangleFlattenedArray.push_back(0);
 
             }
-            // 	cdt->Triangulate();
-            // 	triangles = cdt->GetTriangles();
         }
 
         if(controlPointsUpdated) {
-            // Update VAO/VBO for control points (since we added a new point)
-            // glBindVertexArray(VAO_controlPoints);
-            // glBindBuffer(GL_ARRAY_BUFFER, VBO_controlPoints);
-            // glBufferData(GL_ARRAY_BUFFER, controlPoints.size()*sizeof(GLfloat), &controlPoints[0], GL_DYNAMIC_DRAW);
-            // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-            // glEnableVertexAttribArray(0); //Enable first attribute buffer (vertices)
 
-            glBindVertexArray(VAO_triangles);
-            glBindBuffer(GL_ARRAY_BUFFER, VAO_triangles);
-            glBufferData(GL_ARRAY_BUFFER, triangleFlattenedArray.size()*sizeof(GLfloat), &triangleFlattenedArray[0], GL_STATIC_DRAW);
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);
-            // glEnableVertexAttribArray(static_cast<uint>(vVertex_attrib));
-            // glVertexAttribPointer(static_cast<uint>(vVertex_attrib), 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-            controlPointsUpdated = false; // Finish all VAO/VBO 
+            if (io.MouseDown[0] && !ImGui::IsAnyItemActive()){
+            // // Update VAO/VBO for control points (since we added a new point)
+                glBindVertexArray(VAO_controlPoints);
+                glBindBuffer(GL_ARRAY_BUFFER, VBO_controlPoints);
+                glBufferData(GL_ARRAY_BUFFER, controlPoints.size()*sizeof(GLfloat), &controlPoints[0], GL_DYNAMIC_DRAW);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+                glEnableVertexAttribArray(0); //Enable first attribute buffer (vertices)
+                glBindVertexArray(VAO_controlPoints);
+                glDrawArrays(GL_LINES, 0, controlPoints.size()/3); // Draw points
+                glUseProgram(0);
+                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+                glfwSwapBuffers(window);
+            }
+
+            if (io.MouseReleased[0] &&  !ImGui::IsAnyItemActive()){
+
+                glBindVertexArray(VAO_triangles);
+                glBindBuffer(GL_ARRAY_BUFFER, VAO_triangles);
+                glBufferData(GL_ARRAY_BUFFER, triangleFlattenedArray.size()*sizeof(GLfloat), &triangleFlattenedArray[0], GL_DYNAMIC_DRAW);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+                glEnableVertexAttribArray(0);
+                
+                controlPointsUpdated = false; // Finish all VAO/VBO 
+                glBindVertexArray(VAO_triangles);
+                glDrawArrays(GL_LINE_STRIP, 0, triangleFlattenedArray.size()/3); // Draw points
+                glUseProgram(0);
+                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+                glfwSwapBuffers(window);
+
+            }
         }
-
-        std::cout<<triangleFlattenedArray.size()<<std::endl;
-
         glUseProgram(shaderProgram);
-
-        // Draw control points
-        glBindVertexArray(VAO_triangles);
-		glDrawArrays(GL_LINE_STRIP, 0, triangleFlattenedArray.size()/3); // Draw points
-        glUseProgram(0);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(window);
-
     }
 
+    // glDeleteBuffers(1, &VBO_triangles);
     glDeleteBuffers(1, &VBO_triangles);
+    glDeleteBuffers(1,&VBO_controlPoints);
     //TODO:
 
     // Cleanup
